584a44c2a27b502eccb89f7a815da0178950e47b
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-01-07 12:01:19 +0500

    updated code

---
+++ React/custom-react/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+function App(){
+    return(
+        <h1>
+            <Header />
+            Hello! We are Learning react
+        </h1>
+    )
+}
+
+export default App;
