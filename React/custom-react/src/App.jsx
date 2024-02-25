838af4fb455fe7f3779e6e63eaaf83d472e88596
mabdullahjs <mabdullah2037@outlook.com>
2024-02-25 11:06:24 +0500

    updated code

--- React/custom-react/src/App.jsx
+++ React/custom-react/src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./pages/SearchParams";
 import Details from "./pages/Details";
 function App() {
-
+  const queryClient = new QueryClient();
   return (
     <div>
       <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
 
-        <h1>Adopt Me!</h1>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+          <h1>Adopt Me!</h1>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
       </BrowserRouter>
     </div>
   )
